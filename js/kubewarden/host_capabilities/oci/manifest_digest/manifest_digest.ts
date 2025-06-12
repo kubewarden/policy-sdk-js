9df70ffa0358ece933aa3ae0a7ca60d65fcf808b
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-06-12 14:07:38 -0400

    fix(oci): updated host capability
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- js/kubewarden/host_capabilities/oci/manifest_digest/manifest_digest.ts
+++ js/kubewarden/host_capabilities/oci/manifest_digest/manifest_digest.ts
@@ -8,36 +8,28 @@ export namespace ManifestDigest {
     /**
      * Computes the digest of the OCI object referenced by the image.
      * @param {string} image - The image to be verified (e.g.: `registry.testing.lan/busybox:1.0.0`)
-     * @returns A promise resolving to the image digest string
-     * @throws Error is JSON serialization fails or the host call returns an error
+     * @returns {string} The image digest string
+     * @throws {Error} If JSON serialization fails or the host call returns an error
      */
-
     export function getOCIManifestDigest(image: string): string {
-        // build request payload, e.g: `"ghcr.io/kubewarden/policies/pod-privileged:v0.1.10"`
+        // Serialize the image to JSON
         let payload: ArrayBuffer;
         try {
             payload = new TextEncoder().encode(JSON.stringify(image)).buffer;
         } catch (err) {
-            throw new Error(`cannot serialize image to JSON: ${err}`);
+            throw new Error(`Cannot serialize image to JSON: ${err}`);
         }
 
-        // perform host callback
-        let responsePayload: Uint8Array;
-        try {
-            responsePayload = HostCall.hostCall('kubewarden', 'oci', 'v1/manifest_digest', payload);
-        } catch (err) {
-            throw err;
-        }
-
+        // Perform host call
+        const responsePayload = HostCall.hostCall('kubewarden', 'oci', 'v1/manifest_digest', payload);
         let response: OciManifestResponse;
         try {
             const responseString = new TextDecoder().decode(responsePayload);
             response = JSON.parse(responseString) as OciManifestResponse;
         } catch (err) {
-            throw new Error(`cannot parse response: ${err}`);
+            throw new Error(`Cannot parse response: ${err}`);
         }
 
         return response.digest;
-        
     }
 }
