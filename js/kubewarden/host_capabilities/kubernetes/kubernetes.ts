d21eae814b5e66f4cf0015b98c36b48ba89a9cb3
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-08-06 00:27:08 -0400

    feat: expose apis for `list_resources_by_namespace`, `list_resources_all`, and `can_i`
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- js/kubewarden/host_capabilities/kubernetes/kubernetes.ts
+++ js/kubewarden/host_capabilities/kubernetes/kubernetes.ts
@@ -1,11 +1,73 @@
 import { HostCall } from '../';
 
-import type { GetResourceRequest } from './types';
+import type {
+  GetResourceRequest,
+  ListResourcesByNamespaceRequest,
+  ListAllResourcesRequest,
+  CanIRequest,
+  SubjectAccessReviewStatus,
+} from './types';
 
 /**
- * Kubernetes Host Capability: get_resource
+ * Kubernetes Host Capability
  */
 export namespace Kubernetes {
+  export function listResourcesByNamespace<T = any>(req: ListResourcesByNamespaceRequest): T {
+    let payload: ArrayBuffer;
+    try {
+      payload = new TextEncoder().encode(JSON.stringify(req)).buffer;
+    } catch (err) {
+      throw new Error(`Cannot serialize ListResourcesByNamespaceRequest: ${err}`);
+    }
+
+    let responsePayload: ArrayBuffer;
+    try {
+      responsePayload = HostCall.hostCall(
+        'kubewarden',
+        'kubernetes',
+        'list_resources_by_namespace',
+        payload,
+      );
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+
+    try {
+      const jsonString = new TextDecoder().decode(responsePayload);
+      return JSON.parse(jsonString);
+    } catch (err) {
+      throw new Error(`Failed to decode or parse listResourcesByNamespaceResponse: ${err}`);
+    }
+  }
+
+  export function listAllResources<T = any>(req: ListAllResourcesRequest): T {
+    let payload: ArrayBuffer;
+    try {
+      payload = new TextEncoder().encode(JSON.stringify(req)).buffer;
+    } catch (err) {
+      throw new Error(`Cannot serialize ListAllResourcesRequest: ${err}`);
+    }
+
+    let responsePayload: ArrayBuffer;
+    try {
+      responsePayload = HostCall.hostCall(
+        'kubewarden',
+        'kubernetes',
+        'list_resources_all',
+        payload,
+      );
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+
+    try {
+      const jsonString = new TextDecoder().decode(responsePayload);
+      return JSON.parse(jsonString);
+    } catch (err) {
+      throw new Error(`Failed to decode or parse listAllResourcesResponse: ${err}`);
+    }
+  }
+
   export function getResource<T = any>(req: GetResourceRequest): T {
     let payload: ArrayBuffer;
     try {
@@ -26,6 +88,29 @@ export namespace Kubernetes {
       return JSON.parse(jsonString);
     } catch (err) {
       throw new Error(`Failed to decode or parse getResource response: ${err}`);
+    }
+  }
+
+  export function canI(req: CanIRequest): SubjectAccessReviewStatus {
+    let payload: ArrayBuffer;
+    try {
+      payload = new TextEncoder().encode(JSON.stringify(req)).buffer;
+    } catch (err) {
+      throw new Error(`Cannot serialize CanIRequest: ${err}`);
+    }
+
+    let responsePayload: ArrayBuffer;
+    try {
+      responsePayload = HostCall.hostCall('kubewarden', 'kubernetes', 'can_i', payload);
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+
+    try {
+      const jsonString = new TextDecoder().decode(responsePayload);
+      return JSON.parse(jsonString) as SubjectAccessReviewStatus;
+    } catch (err) {
+      throw new Error(`Failed to decode or parse canI response: ${err}`);
     }
   }
 }
