4e375e1fba607bb10a02616d60988f3d2b19db34
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-08-06 00:26:22 -0400

    feat: added types for `list_resources_by_namespace`, `list_resources_all`, and `can_i`
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- js/kubewarden/host_capabilities/kubernetes/types.ts
+++ js/kubewarden/host_capabilities/kubernetes/types.ts
@@ -1,10 +1,50 @@
-/**
- * Request parameters for the get_resource function
- */
+export interface ListResourcesByNamespaceRequest {
+  api_version: string;
+  kind: string;
+  namespace: string;
+  label_selector?: string | null;
+  field_selector?: string | null;
+}
+
+export interface ListAllResourcesRequest {
+  api_version: string;
+  kind: string;
+  label_selector?: string | null;
+  field_selector?: string | null;
+}
+
 export interface GetResourceRequest {
   api_version: string;
   kind: string;
   name: string;
   namespace?: string | null;
   disable_cache: boolean;
+}
+
+export interface CanIRequest {
+  subject_access_review: SubjectAccessReview;
+  disable_cache: boolean;
+}
+
+export interface SubjectAccessReview {
+  groups?: string[];
+  resource_attributes: ResourceAttributes;
+  user: string;
+}
+
+export interface ResourceAttributes {
+  group?: string;
+  name?: string;
+  namespace?: string;
+  resource: string;
+  subresource?: string;
+  verb: string;
+  version?: string;
+}
+
+export interface SubjectAccessReviewStatus {
+  allowed: boolean;
+  denied?: boolean;
+  reason?: string;
+  evaluationError?: string;
 }
