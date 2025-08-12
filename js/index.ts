b7951d12b244dda7f78cd5d95c8d7078f75ed7bf
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-08-12 07:22:39 -0400

    feat: distribute kubewarden's javy plugin as part of the sdk
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- js/index.ts
+++ js/index.ts
@@ -5,19 +5,37 @@ import * as hostCapabilities from './kubewarden/host_capabilities';
 import * as validation from './kubewarden/validation';
 import * as protocol from './protocol';
 
+// import path only in Node.js environment
+let pluginPath: string | undefined;
+try {
+  // this will only work in Node.js, not in WASM
+  if (typeof __dirname !== 'undefined') {
+    const path = require('path');
+    pluginPath = path.resolve(__dirname, '../plugin/javy-plugin-kubewarden.wasm');
+  }
+} catch {
+  // in WASM environment, pluginPath remains undefined
+  pluginPath = undefined;
+}
+
 // Export
-export default {
+const exported = {
   admission,
   validation,
   hostCapabilities,
   constants,
   protocol,
+  ...(pluginPath && { pluginPath }), // only include pluginPath if it exists
 };
 
+export default exported;
+
+// Named exports
 export { validation as Validation };
 export { protocol };
 export { admission };
 export { constants };
 export { hostCapabilities };
+export { pluginPath }; // undefined in WASM
 
 export const { writeOutput } = protocol;
