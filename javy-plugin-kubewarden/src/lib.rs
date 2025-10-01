067644c1539e3548d15d6837a625a99cf306f57c
Flavio Castelli <fcastelli@suse.com>
2025-10-01 18:10:15 +0200

    refactor: huge change to use javy-plugin 4.0
    Move to javy-plugin 4.0. This version requires javy plugins to be
    written for the wasip2 target. That requires to use the WebAssembly
    component model.
    
    This PR introduces WIT definitions for the javy plugin, plus the import
    function that is required by Kubewarden WASI policies.
    
    The WIT definitions make use of the new rich data types provided by the
    WIT specification.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- javy-plugin-kubewarden/src/lib.rs
+++ javy-plugin-kubewarden/src/lib.rs
@@ -1,75 +1,64 @@
-use javy_plugin_api::javy::quickjs::{prelude::Func, Error, Object};
-use javy_plugin_api::{import_namespace, Config};
+use javy_plugin_api::{
+    javy::{
+        quickjs::{prelude::Func, Error, Object},
+        Runtime,
+    },
+    javy_plugin, Config,
+};
 
-import_namespace!("kubewarden");
+wit_bindgen::generate!({
+    world: "kubewarden-plugin",
+    generate_all
+});
 
-#[link(wasm_import_module = "host")]
-extern "C" {
-    /// The host's exported __host_call function.
-    pub(crate) fn call(
-        bd_ptr: *const u8,
-        bd_len: usize,
-        ns_ptr: *const u8,
-        ns_len: usize,
-        op_ptr: *const u8,
-        op_len: usize,
-        ptr: *const u8,
-        len: usize,
-    ) -> usize;
-}
-
-#[export_name = "initialize_runtime"]
-pub extern "C" fn initialize_runtime() {
+fn config() -> Config {
     let mut config = Config::default();
     config.text_encoding(true).javy_stream_io(true);
 
-    javy_plugin_api::initialize_runtime(config, |runtime| {
-        runtime
-            .context()
-            .with(|ctx| {
-                ctx.globals().set(
-                    "policyAction",
-                    Func::from(|| {
-                        let args = std::env::args().collect::<Vec<String>>();
-                        if args.len() != 2 {
-                            // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
-                            return Err(Error::Unknown);
-                        }
-                        Ok(args[1].clone())
-                    }),
-                )
-            })
-            .unwrap();
-        runtime
-            .context()
-            .with(|ctx| {
-                ctx.globals().set(
-                    "__hostCall",
-                    Func::from(|binding: String, ns: String, op: String, msg: Object| {
-                        let msg = msg
-                            .as_array_buffer()
-                            .and_then(|ab| ab.as_bytes())
-                            .ok_or(Error::Unknown)?; // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
-
-                        let successful = unsafe {
-                            call(
-                                binding.as_ptr(),
-                                binding.len(),
-                                ns.as_ptr(),
-                                ns.len(),
-                                op.as_ptr(),
-                                op.len(),
-                                msg.as_ptr(),
-                                msg.len(),
-                            )
-                        };
-
-                        Ok::<bool, Error>(successful == 0)
-                    }),
-                )
-            })
-            .unwrap();
-        runtime
-    })
-    .unwrap();
+    config
 }
+
+fn modify_runtime(runtime: Runtime) -> Runtime {
+    runtime
+        .context()
+        .with(|ctx| {
+            ctx.globals().set(
+                "policyAction",
+                Func::from(|| {
+                    let args = std::env::args().collect::<Vec<String>>();
+                    if args.len() != 2 {
+                        // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
+                        return Err(Error::Unknown);
+                    }
+                    Ok(args[1].clone())
+                }),
+            )
+        })
+        .unwrap();
+    runtime
+        .context()
+        .with(|ctx| {
+            ctx.globals().set(
+                "__hostCall",
+                Func::from(|binding: String, ns: String, op: String, msg: Object| {
+                    let msg = msg
+                        .as_array_buffer()
+                        .and_then(|ab| ab.as_bytes())
+                        .ok_or(Error::Unknown)?; // TODO: move to Error::UserData when javy upgrades to latest version of rquickjs
+
+                    crate::kubewarden::wasip2::host::call(&binding, &ns, &op, msg)
+                        .map_err(|_| Error::Unknown)
+                }),
+            )
+        })
+        .unwrap();
+    runtime
+}
+
+struct Component;
+
+// Dynamically linked modules will use `my_javy_plugin_v1` as the import
+// namespace.
+javy_plugin!("kubewarden-plugin", Component, config, modify_runtime);
+
+export!(Component);
